27c51a338abef532bd2721b95a12cb62553b4c4f
natogourmet <natogourmet@hotmail.com>
2022-08-17 23:50:55 -0500

    express with TS

--- index.ts
+++ index.ts
@@ -1,8 +1,16 @@
-const express = require('express')
+import express from 'express';
+
+// const express = require('express')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
+
+  res.status(201).json({
+    ok: false,
+    msg: 'Well done'
+  })
+  
   res.json({
     message: 'Hello World!'
   })
